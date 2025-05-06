b7996296310a145eed38c2147ca89f020a53b7fc
MatW3B <mateusz343maj@gmail.com>
2025-05-06 22:05:35 +0200

    feat(dataSchema): Add required description to drinks schema and add subtitle to each drinks section

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,7 +10,7 @@ const drinks = defineCollection({
         isAlcoholic: z.boolean(),
         title: z.string(),
         ingredients: z.array(z.string()),
-        taste: z.array(z.string()),
+        description: z.string({required_error: 'Description is required'}),
         intensity: z.number(),
     }),
 });
