916a11a4911b26ab3e54c4f8d055f897741f01b2
MatW3B <mateusz343maj@gmail.com>
2025-05-07 22:42:17 +0200

    feat(icons): Add possibility to use any open sourced icons distributed by Iconify package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [vue(), icon({
+    include: {
+      'mdi': ['glass-tulip'],
+    }
+  })]
 });
