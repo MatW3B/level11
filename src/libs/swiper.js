c4f11d71854350b84184c56c7349d6ea9a576fb5
MatW3B <mateusz343maj@gmail.com>
2025-05-17 02:28:58 +0200

    feat(Swiper): Add swipper library to accommodate overflowing content. - Implemented scrollbar container approach due to relative ease of implementation and keeping the user well informed about the content on the page with sleek, non intruisive look. - Fixed container's heights not to overflow past body container

---
+++ src/libs/swiper.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper';
+import { FreeMode, Scrollbar } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/scrollbar';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper(".drinksSwiper", {
+        modules: [FreeMode, Scrollbar],
+        direction: "vertical",
+        slidesPerView: "auto",
+        freeMode: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+            dragSize: 400
+        },
+    });
+});
